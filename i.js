3cf62044478ccb06e823c673321f614f32614e81
familoff <max.familoff@ya.ru>
2024-01-25 15:13:30 +0300

    Update i.js

--- i.js
+++ i.js
@@ -84,43 +84,63 @@
 		// 	this.background = function () {
 		// 	  Lampa.Background.immediately('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAZCAYAAABD2GxlAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAHASURBVHgBlZaLrsMgDENXxAf3/9XHFdXNZLm2YZHQymPk4CS0277v9+ffrut62nEcn/M8nzb69cxj6le1+75f/RqrZ9fatm3F9wwMR7yhawilNke4Gis/7j9srQbdaVFBnkcQ1WrfgmIIBcTrvgqqsKiTzvpOQbUnAykVW4VVqZXyyDllYFSKx9QaVrO7nGJIB63g+FAq/xhcHWBYdwCsmAtvFZUKE0MlVZWCT4idOlyhTp3K35R/6Nzlq0uBnsKWlEzgSh1VGJxv6rmpXMO7EK+XWUPnDFRWqitQFeY2UyZVryuWlI8ulLgGf19FooAUwC9gCWLcwzWPb7Wa60qdlZxjx6ooUuUqVQsK+y1VoAJyBeJAVsLJeYmg/RIXdG2kPhwYPBUQQyYF0XC8lwP3MTCrYAXB88556peCbUUZV7WccwkUQfCZC4PXdA5hKhSVhythZqjZM0J39w5m8BRadKAcrsIpNZsLIYdOqcZ9hExhZ1MH+QL+ciFzXzmYhZr/M6yUUwp2dp5U4naZDwAF5JRSefdScJZ3SkU0nl8xpaAy+7ml1EqvMXSs1HRrZ9bc3eZUSXmGa/mdyjbmqyX7A9RaYQa9IRJ0AAAAAElFTkSuQmCC');
 		// 	};
-		this.start = function () {
-			if (Lampa.Activity.active().activity !== this.activity) return;
-			this.background();
-			Lampa.Controller.add('content', {
-				toggle: function toggle() {
-					Lampa.Controller.collectionSet(scroll.render());
-					Lampa.Controller.collectionFocus(last || false, scroll.render());
+		this.start = function(){
+			if(items.length && Activity.active().activity == this.activity) items[0].toggleBackground()
+	
+			Controller.add('content',{
+				update: ()=>{},
+				toggle: ()=>{
+					if(items.length){
+						items[active].toggle()
+					}
+					else{
+						Controller.collectionSet(scroll.render())
+						Controller.collectionFocus(false,scroll.render())
+					}
 				},
-				left: function left() {
-					if (Navigator.canmove('left')) Navigator.move('left'); else Lampa.Controller.toggle('menu');
+				left: ()=>{
+					Controller.toggle('menu')
 				},
-				right: function right() {
-					Navigator.move('right');
+				up: ()=>{
+					Controller.toggle('head')
 				},
-				up: function up() {
-					if (Navigator.canmove('up')) Navigator.move('up'); else Lampa.Controller.toggle('head');
-				},
-				down: function down() {
-					if (Navigator.canmove('down')) Navigator.move('down');
-				},
-				back: this.back
-			});
-			Lampa.Controller.toggle('content');
-		};
-		this.pause = function () { };
-		this.stop = function () { };
-		this.render = function () {
-			return html;
-		};
-		this.destroy = function () {
-			network.clear();
-			lampa.arrays.destroy(items);
-			scroll.destroy();
-			html.remove();
-			items = null;
-			network = null;
-		};
+				back: ()=>{
+					Activity.backward()
+				}
+			})
+	
+			Controller.toggle('content')
+		}
+	
+		this.pause = function(){
+			
+		}
+	
+		this.stop = function(){
+			
+		}
+	
+		this.render = function(){
+			return html
+		}
+	
+		this.destroy = function(){
+			network.clear()
+	
+			Arrays.destroy(items)
+	
+			scroll.destroy()
+	
+			html.remove()
+	
+			items = null
+			network = null
+	
+			if(background_image){
+				background_image.onload = ()=>{}
+				background_image.src = ''
+			}
+		}
 	}
 
 	function startPlugin() {
